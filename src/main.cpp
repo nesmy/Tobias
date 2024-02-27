c85f76bee3b5dd232f279688306ae406a2a0f5ca
Nesmy Point du Jour <pnesmy@gmail.com>
2024-02-27 18:06:46 -0500

    android

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,18 @@
-#include <espeak-ng/speak_lib.h>
-#include <iostream>
-
-espeak_AUDIO_OUTPUT output = AUDIO_OUTPUT_SYNCH_PLAYBACK;
-char *path = NULL;
-void *user_data;
-unsigned int *identifier;
+#include "Ai.h"
+#include <cstdio>
+// #include <tensorflow/c/c_api.h>
 
 int main(int argc, char *argv[]) {
-  char voicename[] = {"English"}; // Set voice by its name
-  char text[] = {"Hello Nesmy!"};
-  int buflength = 500, options = 0;
-  unsigned int position = 0, position_type = 0, end_position = 0,
-               flags = espeakCHARS_AUTO;
-  espeak_Initialize(output, buflength, path, options);
-  espeak_SetVoiceByName(voicename);
-  printf("Saying  '%s'...\n", text);
-  espeak_Synth(text, buflength, position, espeak_POSITION_TYPE::POS_WORD,
-               end_position, flags, identifier, user_data);
-  printf("Done\n");
+  // printf("Hello from the other side %s\n", TF_Version());
+  char text[] = {"Hello world!"};
+  AI TobiasAI;
+  // TobiasAI.Say(argv[1]);
+  // TobiasAI.Say(argv[2]);
+
+  int close = 0;
+  TobiasAI.Say(text);
+  TobiasAI.listen();
+
+  TobiasAI.Shutdown();
   return 0;
 }
